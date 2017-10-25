8cfc7415bb28b86632b8d45d580d8551460bc6a8
Iann Singer <itpsinger@gmail.com>
2017-10-25 00:44:03 +0000

    Added user ids and emails to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
    @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
@@ -6,14 +7,16 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+
   def create
+    current_user.places.create(place_params)
+    redirect_to root_path
   end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
-    redirect_to root_path
   end
 
 
